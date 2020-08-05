cc661c15ad84c51cb45ee1a120cf534e412a3f87
Raahim fareed <Raahim Fareed>
2020-08-06 01:50:53 +0500

    Init

--- cogs/staff.py
+++ cogs/staff.py
@@ -1,4 +1,3 @@
-from distutils.command import check
 import discord
 from discord.ext import commands
 
@@ -10,35 +9,58 @@ import os
 class Staff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bot_id = bot.id
+        self.bot_id = os.environ.get("BOT_ID")
         self.owner_id = os.environ.get("OWNER_ID")
+
+    @commands.group()
+    @commands.has_permissions(manage_roles=True)
+    async def role(self, ctx):
+        pass
+
+    @role.command(name="create")
+    async def _create(self, ctx, name):
+        await ctx.guild.create_role(name=name)
+        bots_message = await ctx.send(f"Role `{name}` created.")
+        await asyncio.sleep(5)
+        await ctx.message.delete()
+        await bots_message.delete()
+
+    @role.command(name="delete")
+    async def _delete(self, ctx, role: discord.Role, *, reason=""):
+        bots_question = await ctx.send("Are you sure you want to delete this role? Y/N")
+
+        msg = await self.bot.wait_for("message", check=lambda x: x.author == ctx.message.author)
+
+        if msg and msg is not None:
+            if msg.content.lower() == "y" or msg.content.lower() == "yes":
+                await role.delete(reason=reason)
+                bots_reply = await ctx.send(f"{role.name} deleted.")
+            elif msg.content.lower() == "n" or msg.content.lower() == "no":
+                bots_reply = await ctx.send(f"{role.name} not deleted.")
+            else:
+                bots_reply = await ctx.send("Invalid reply! Role not deleted.")
+
+            await asyncio.sleep(5)
+            await ctx.message.delete()
+            await bots_question.delete()
+            await msg.delete()
+            await bots_reply.delete()
+
+    @role.command(name="give")
+    async def _give(self, ctx, mention: discord.Member, role: discord.Role):
+        if role in mention.roles:
+            await mention.remove_roles(role)
+            await ctx.send(f"{role.name} removed from {mention.mention}")
+        else:
+            await mention.add_roles(role)
+            await ctx.send(f"{mention.mention} assigned {role.name}")
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=5):
         await ctx.channel.purge(limit=amount + 1)
 
-    @commands.command()
-    async def create(self, ctx, create_type):
-        permissions = ctx.author.permissions_in(ctx.channel)
-        create_type = create_type.lower()
-
-        def check_message(m):
-            return m.author != self.bot
-
-        if permissions.manage_roles or permissions.manage_channels:
-            if create_type == "role":
-                pass
-            elif create_type == "text":
-                pass
-            elif create_type == "voice":
-                pass
-        else:
-            bots_message = await ctx.send("I think not")
-            await asyncio.sleep(7)
-            await ctx.message.delete()
-            await bots_message.delete()
-
+    # Incomplete
     @commands.command()
     @commands.has_permissions(manage_permissions=True)
     async def channel(self, ctx, channel_type, channel: discord.TextChannel):
